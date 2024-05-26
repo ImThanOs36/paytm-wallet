22d47c26b0c9855b8d2f9d37be3f59bb3550466e
ImThanOs36 <lads42279@gmail.com>
2024-05-26 17:50:20 +0530

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+
+      '/':"https://paytm-wallet-w3ys.vercel.app"
+    }
+  },
   plugins: [react()],
 })
