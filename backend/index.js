6d9e85fc3d408ab48bc6a1e5ffa5aa353844abf4
ImThanOs36 <lads42279@gmail.com>
2024-07-02 13:10:00 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const app = express();
-const cors=require('cors')
+const cors = require('cors')
 const PORT = 5000;
-const userRoutes = require("./routes/userRoutes") 
+const userRoutes = require("./routes/userRoutes")
 const accountRoutes = require("./routes/accountRoutes")
 const bodyParser = require("body-parser")
- 
-app.use(express(),cors(),bodyParser());
-app.use("/user",userRoutes)
-app.use("/account",accountRoutes)
-   app.get("/get", (req,res)=>{
+app.use(cors({origin:"*"}))
+app.use(express(), bodyParser());
+app.use("/user", userRoutes)
+app.use("/account", accountRoutes)
+app.get("/get", (req, res) => {
 
     console.log("connected")
-    res.json({msg:"working"})
-   })
-app.listen(PORT,()=>{
+    res.json({ msg: "working" })
+})
+app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 })
